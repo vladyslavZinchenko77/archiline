9d7265ddd6549438ee18cc38593c2a36fe8ef34a
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-07 15:54:13 +0200

    added section team

---
+++ src/components/ui/Team/Team.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react';
+
+import './Team.scss';
+
+const Team: FC = () => {
+  return (
+    <section className="team">
+      <div className="team__wrap">
+        <div className="team__text">
+          <h2 className="team__text-title">Meet our team</h2>
+          <p className="team__text-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            varius enim in eros elementum tristique. Duis cursus, mi quis.
+          </p>
+        </div>
+        <ul className="team__list">
+          <li className="team__list-item"></li>
+          <li className="team__list-item"></li>
+          <li className="team__list-item"></li>
+          <li className="team__list-item"></li>
+          <li className="team__list-item"></li>
+          <li className="team__list-item"></li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
