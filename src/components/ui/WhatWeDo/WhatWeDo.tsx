b2413432046eff45252189be01b4b6ee5304adef
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-11 16:30:17 +0200

    updated what we do section, added icons

--- src/components/ui/WhatWeDo/WhatWeDo.tsx
+++ src/components/ui/WhatWeDo/WhatWeDo.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from 'react';
 
+import Image from 'next/image';
+
 import './WhatWeDo.scss';
 
 const WhatWeDo: FC = () => {
@@ -9,9 +11,35 @@ const WhatWeDo: FC = () => {
         <p className="general-text wedo__text">Our process</p>
         <h2 className="general-title wedo__title">How we do what we do.</h2>
         <ul className="wedo__list">
-          <li className="wedo__list-item">Commercial buildings</li>
-          <li className="wedo__list-item">Residential apartment buildings</li>
-          <li className="wedo__list-item">Individual houses</li>
+          <li className="wedo__list-item">
+            <Image
+              width={50}
+              height={50}
+              src={'/svg/shop.svg'}
+              alt="Commercial buildings"
+            />
+            <p className="wedo__list-item--text">Commercial buildings</p>
+          </li>
+          <li className="wedo__list-item">
+            <Image
+              width={50}
+              height={50}
+              src={'/svg/commerce.svg'}
+              alt="Residential apartment buildings"
+            />
+            <p className="wedo__list-item--text">
+              Residential apartment buildings
+            </p>
+          </li>
+          <li className="wedo__list-item">
+            <Image
+              width={50}
+              height={50}
+              src={'/svg/house.svg'}
+              alt="Residential apartment buildings"
+            />
+            <p className="wedo__list-item--text">Individual houses</p>
+          </li>
         </ul>
       </div>
     </section>
