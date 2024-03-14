57e0a719db5ac1c57e98708a6ec7d2db06918b75
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-14 19:28:03 +0200

    made our team section addaptive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,8 +42,8 @@ export default function Home() {
           alt="interior"
         />
         <Projects />
-        {/* <Team />
-        <NextProject />
+        <Team />
+        {/* <NextProject />
         <Footer /> */}
       </main>
     </>
