8b759b2a4f2515ff2514f6de340f829d11cedcb7
Vlad Zinchenko <vlad.zinchenko@bndigital.co>
2024-03-18 19:53:45 +0200

    added contacts page content

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -1,7 +1,41 @@
 import React, { FC } from 'react';
+import { Container, Typography, Grid, TextField, Button } from '@mui/material';
 
 const Contacts: FC = () => {
-  return <div>contacts</div>;
+  return (
+    <Container>
+      <Typography style={{ marginTop: 24 }} variant="h4" gutterBottom>
+        Contact Us
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h6">Contact Information</Typography>
+          <Typography>Email: archiline.ab@gmail.com</Typography>
+          <Typography>Phone: +38 098 653 8227</Typography>
+          <Typography>
+            Address: 57 Poltavska St, Office 97, Irpin, Kiev Oblast, Ukraine{' '}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <form noValidate autoComplete="off">
+            <TextField id="name" label="Name" fullWidth margin="normal" />
+            <TextField id="email" label="Email" fullWidth margin="normal" />
+            <TextField
+              id="message"
+              label="Message"
+              multiline
+              rows={4}
+              fullWidth
+              margin="normal"
+            />
+            <Button variant="contained" color="primary" fullWidth>
+              Send
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Contacts;
